cdc08e72b2a3b29d7ea3ad6baba8e6891a7239ab
Strange-can <179990912+Strange-can@users.noreply.github.com>
2024-09-15 14:48:59 +0800

    Add .textContent = '' at start of function
    Button now calls create() function and create() replaces all child elements with 'null' before creating the new set of grids,
    grids now only get created and replaced in the container. added 15px gap between button and grid for visuals.

--- js-file.js
+++ js-file.js
@@ -2,45 +2,35 @@ const container = document.querySelector(".container")
 let square = document.createElement("div")
 const button = document.querySelector("button")
 
-let num = window.prompt('how many squares per side do you want?', 16)
-
-function draw() {
-for (num;;) {
-if (num > 100) {
-    num = window.prompt('number is too high, choose between 1 to 100')
-}
-else if (num > 0 && num <= 100)
-  break
-else {
-  num = window.prompt('inavild input, choose a number between 1 to 100')
-}
-}
-
-
-squaredNum = num * num
-let length = Math.sqrt(250000/squaredNum)
-for (i = 1; i <= squaredNum; i++) {
-  const square = document.createElement("div")
-  square.classList.add("square")
-  square.style.height = `${length}px`
-  square.style.width = `${length}px`
-  container.appendChild(square)
-  square.addEventListener("mouseenter", () => {
-    let color = Math.random()
-    if (color <= 0.33) {
-    square.style.backgroundColor = "green"
-    }
-    else if (color <= 0.66) {
-      square.style.backgroundColor = "blue"
+function create(num) {
+container.textContent = ''
+if (num > 0 && num <= 100) {
+  squaredNum = num * num
+  let length = Math.sqrt(250000 / squaredNum)
+  for (i = 1; i <= squaredNum; i++) {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.style.height = `${length}px`
+    square.style.width = `${length}px`
+    container.appendChild(square)
+    square.addEventListener("mouseenter", () => {
+      let color = Math.random()
+      if (color <= 0.33) {
+        square.style.backgroundColor = "green"
+      }
+      else if (color <= 0.66) {
+        square.style.backgroundColor = "blue"
       }
     else {
       square.style.backgroundColor = "yellow"
-      }
-  })
+      }})}}
+else {
+    num = window.prompt('invalid input, choose between 1 to 100')
+    return create(num)
 }
 }
-
-draw()
-
-/*button.addEventListener("click", 
-() => num = window.prompt('how many squares per side do you want?', 16), draw())*/
+button.addEventListener("click", function () {
+  num = window.prompt('how many squares per side do you want?(1 to 100)', 16)
+  create(num)
+})
+create(16)
